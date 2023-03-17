f34057e6000f2ed4476d9bd584d09a78fdfb99a9
Alesnguyen <128109817+Alesnguyen@users.noreply.github.com>
2023-03-17 10:14:24 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,28 +3,23 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: 'niviki-com-t-do-t-i-ch-nh-10f5e54221eb4b059bc5b5305da949ba',
-  rootNotionPageId: 'Simple-Template-49955b133eb843bdb8665534ecfd844c',
+  rootNotionPageId: 'VNPETRO-Digital-Asset-Management-a3bd19d54f214f3bbf88a89d4e2b9888',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'NIVIKI.COM - TỰ DO TÀI CHÍNH',
-  domain: 'niviki.com',
-  author: 'Khoa Nguyen',
+  name: 'VNPETRO Digital Asset Management',
+  domain: 'vnpetro.com.vn',
+  author: 'Alesnguyen',
 
   // open graph metadata (optional)
-  description: 'Lan toả tinh thần tự do tài chính đến với nhiều người hơn nữa! Tự do tài chính là bạn được quyền “thất nghiệp” nhưng vẫn có đủ tiền cho một cuộc sống thoải mái trong thời gian dài',
+  description: 'Dễ dàng tìm kiếm tài liệu về quy trình, chính sách, nội quy, ấn phẩm,... với hệ thống tài nguyên nội bộ (VNPETRO DAM)',
 
-  // social usernames (optional)
-  twitter: 'khoa_nvk',
-  // github: 'transitive-bullshit',
-  linkedin: 'khoanguyen08',
-  
-  facebook: 'https://www.facebook.com/groups/nocodevietnam',
+  facebook: 'https://www.facebook.com/VNPETRO.official/',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'https://www.youtube.com/@vnpetro5666', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
